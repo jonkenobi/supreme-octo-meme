1b02f84839055a09e5ae1caa36b7a2796e3ac024
jonkenobi <40736290+jonkenobi@users.noreply.github.com>
2020-12-30 23:02:19 +0900

    Add input sanitization and validation

--- main.py
+++ main.py
@@ -3,11 +3,17 @@
 # Uncomment the inputs to input interactively
 import requests
 
-# PATCH_VERSION = input("Enter patch version ")
-PATCH_VERSION = "8.2.1"
-# versions_url = "https://ddragon.leagueoflegends.com/api/versions.json"
-# CHAMPION_NAME = input("Enter champion name")
-CHAMPION_NAME = "Aatrox"
+versions_url = "https://ddragon.leagueoflegends.com/api/versions.json"
+response = requests.get(versions_url)
+response.raise_for_status()
+VALID_PATCH_VERSIONS = response.json()
+
+PATCH_VERSION = ""
+while PATCH_VERSION not in VALID_PATCH_VERSIONS:
+    PATCH_VERSION = str(input("Enter valid patch version: ").strip())
+
+CHAMPION_NAME = str(input("Enter champion name: ").strip())
+# CHAMPION_NAME = "Aatrox"
 
 url = "http://ddragon.leagueoflegends.com/cdn/" + PATCH_VERSION + "/data/en_US/champion/" + CHAMPION_NAME + ".json"
 response = requests.get(url)
@@ -16,4 +22,4 @@ response.raise_for_status()
 raw_champion_json_data = response.json()
 spells = raw_champion_json_data["data"][CHAMPION_NAME]["spells"]
 q_spell = spells[0]
-print("Q skill name for ", CHAMPION_NAME, " is ", q_spell["name"])
+print("Q skill name for ", CHAMPION_NAME, " at patch ", PATCH_VERSION, " is ", q_spell["name"])
